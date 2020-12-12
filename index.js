dc4854ee4472e6b2e4f4a168834127a4b148b26f
JesusfGarcia <jesusfgarciavaldez@gmail.com>
2020-12-11 17:35:50 -0700

    js basics

---
+++ index.js
@@ -0,0 +1,35 @@
+//const num1 = 5;
+/*const num2 = 5;
+
+console.log("suma => ", num1 + num2);
+console.log("restar => ", num1 - num2);
+console.log("multiplicar => ", num1 * num2);
+console.log("dividir => ", num1 / num2);
+console.log("residuo => ", num1 % num2);
+console.log("igual => ", num1 === num2);*/
+
+/*const function2 = (num1, num2) => {
+  if (num1 === num2) {
+    console.log("son iguales");
+  } else if (num1 > num2) {
+    console.log("número 1 es mayor");
+  } else {
+    console.log("número 2 es mayor");
+  }
+};
+
+function2(10, 10)*/
+
+/*const myArray = ["jesus", "grabiel", "juanita"];
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}*/
+
+//const myArray = ["jesus", "grabiel", "juanita"];
+
+/*for (let i = 0; i < myArray.length; i++) {
+  for (let j = 0; j < myArray[i].length; j++) {
+    console.log(myArray[i][j]);
+  }
+}*/
